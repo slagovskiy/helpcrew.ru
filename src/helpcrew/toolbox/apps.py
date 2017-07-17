d3af61fc597f9f56876014640909650f01c95b6d
Sergey Lagovskiy <slagovskiy@gmail.com>
2017-07-17 11:50:48 +0700

    startup for global

--- src/helpcrew/toolbox/apps.py
+++ src/helpcrew/toolbox/apps.py
@@ -2,4 +2,9 @@ from django.apps import AppConfig
 
 
 class ToolboxConfig(AppConfig):
-    name = 'odyssey.toolbox'
+    name = 'toolbox'
+
+    def ready(self):
+        from toolbox.models import Global
+        Global.add_not_exist('user-crew-limit', '3')
+        Global.add_not_exist('crew-user-limit', '10')
