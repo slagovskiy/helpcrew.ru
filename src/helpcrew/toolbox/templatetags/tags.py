6d163cc600084baf9a9dbbebd64a2f02bdec3179
Sergey Lagovskiy <slagovskiy@gmail.com>
2017-07-25 16:32:34 +0700

    tags member check

--- src/helpcrew/toolbox/templatetags/tags.py
+++ src/helpcrew/toolbox/templatetags/tags.py
@@ -1,6 +1,8 @@
 import uuid
 from django import template
 
+from ...helpdesk.utils import check_member_admin, check_member_dispatcher, check_member
+
 register = template.Library()
 
 
@@ -8,3 +10,21 @@ register = template.Library()
 def date_from_now(val):
     u = str(uuid.uuid4())
     return '<span id="date_' + u + '"><script>moment.locale(\'ru\');$("#date_' + u + '").html(moment("%s").fromNow());\n</script></span>' % val
+
+
+@register.simple_tag(takes_context=True)
+def chk_member_admin(context, crew):
+    request = context['request']
+    return check_member_admin(request.user, crew)
+
+
+@register.simple_tag(takes_context=True)
+def chk_member_dispatcher(context, crew):
+    request = context['request']
+    return check_member_dispatcher(request.user, crew)
+
+
+@register.simple_tag(takes_context=True)
+def chk_member(context, crew):
+    request = context['request']
+    return check_member(request.user, crew)
