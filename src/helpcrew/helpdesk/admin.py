70aa9bc41e0f03832d02beb1325db7e45c69dc04
Sergey Lagovskiy <slagovskiy@gmail.com>
2017-08-07 16:47:45 +0700

    crew service admin

--- src/helpcrew/helpdesk/admin.py
+++ src/helpcrew/helpdesk/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Crew, CrewUsers, CrewService
+from .models import Crew, CrewUsers, CrewService, ServicePrice
 
 
 class CrewAdmin(admin.ModelAdmin):
@@ -24,6 +24,14 @@ class CrewServiceAdmin(admin.ModelAdmin):
     readonly_fields = ()
 
 
+class ServicePriceAdmin(admin.ModelAdmin):
+    list_display = ('service', 'start_date', 'cost')
+    ordering = ('service', 'start_date')
+    exclude = ()
+    readonly_fields = ()
+
+
 admin.site.register(CrewUsers, CrewUsersAdmin)
 admin.site.register(CrewService, CrewServiceAdmin)
+admin.site.register(ServicePrice, ServicePriceAdmin)
 admin.site.register(Crew, CrewAdmin)
