4742d9b878506a30275d8fb300c48403d70ce824
Sergey Lagovskiy <slagovskiy@gmail.com>
2017-09-04 23:09:51 +0700

    field model

--- src/helpcrew/dyntable/models.py
+++ src/helpcrew/dyntable/models.py
@@ -34,3 +34,57 @@ class Table(models.Model):
         ordering = ['name']
         verbose_name = u'Динамическая таблица'
         verbose_name_plural = u'Динамические таблицы'
+
+
+class Field(models.Model):
+    TYPE_NUMERIC = 1
+    TYPE_STRING = 2
+    TYPE_DATE = 3
+    TYPE_IP = 4
+    TYPE_PASSWORD = 5
+    TYPES = (
+        (TYPE_NUMERIC, 'Число'),
+        (TYPE_STRING, 'Строка'),
+        (TYPE_DATE, 'Дата'),
+        (TYPE_IP, 'IP'),
+        (TYPE_PASSWORD, 'Пароль')
+    )
+    table = models.ForeignKey(
+        Table,
+        verbose_name=u'Таблица'
+    )
+    uuid = models.SlugField(
+        unique=True,
+        verbose_name=u'Ключ'
+    )
+    name = models.CharField(
+        max_length=50,
+        default='',
+        verbose_name=u'Наименование поля'
+    )
+    type = models.IntegerField(
+        choices=TYPES,
+        default=TYPE_STRING,
+        verbose_name=u'Тип поля'
+    )
+    order = models.IntegerField(
+        default=10,
+        verbose_name=u'Сортировка'
+    )
+    deleted = models.BooleanField(
+        default=False,
+        verbose_name=u'Запись удалена'
+    )
+
+    def __str__(self):
+        return '%s %s' % (self.name, self.table.name)
+
+    def save(self, *args, **kwargs):
+        if not self.uuid:
+            self.uuid = uuid.uuid1()
+        super(Field, self).save(*args, **kwargs)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = u'Поле таблицы'
+        verbose_name_plural = u'Поля таблицы'
