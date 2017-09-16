19fca17811f0d852a4704e6aad8d2dbd9a48f877
Sergey Lagovskiy <slagovskiy@gmail.com>
2017-09-16 13:36:05 +0700

    field api

--- src/helpcrew/dyntable/urls.py
+++ src/helpcrew/dyntable/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 
 from .views import api_table_list, api_table_edit, api_table_delete
+from .views import api_field_list, api_field_edit, api_field_delete
 
 urlpatterns = [
     url(r'^api/table/list/$', api_table_list, name='api_table_list'),
@@ -9,4 +10,11 @@ urlpatterns = [
     url(r'^api/table/edit/(?P<table>[-\w]+)/$', api_table_edit, name='api_table_edit'),
     url(r'^api/table/delete/$', api_table_delete, name='api_table_delete'),
     url(r'^api/table/delete/(?P<table>[-\w]+)/?$', api_table_delete, name='api_table_delete'),
+
+    url(r'^api/field/list/$', api_field_list, name='api_field_list'),
+    url(r'^api/field/list/(?P<table>[-\w]+)/$', api_field_list, name='api_field_list'),
+    url(r'^api/field/edit/$', api_field_edit, name='api_field_edit'),
+    url(r'^api/field/edit/(?P<field>[-\w]+)/$', api_field_edit, name='api_field_edit'),
+    url(r'^api/field/delete/$', api_field_delete, name='api_field_delete'),
+    url(r'^api/field/delete/(?P<field>[-\w]+)/?$', api_field_delete, name='api_field_delete'),
 ]
--- src/helpcrew/dyntable/views.py
+++ src/helpcrew/dyntable/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 
-from .models import Table
+from .models import Table, Field
 from ..helpdesk.utils import check_member, check_member_admin
 from ..helpdesk.models import Crew, CrewEvent
 
@@ -67,16 +67,16 @@ def api_table_edit(request, table=None):
             else:
                 return HttpResponse('crew not found!')
         table = Table.objects.filter(id=table).first()
-        if check_member_admin(request.user, table.crew):
-            if table:
+        if table:
+            if check_member_admin(request.user, table.crew):
                 table.name = request.POST.get('name', '_')
                 table.save()
-                CrewEvent.addEvent(request, table.crew, u'Изменен приоритет ' + table.name)
+                CrewEvent.addEvent(request, table.crew, u'Изменен справочник ' + table.name)
                 return HttpResponse('ok')
             else:
-                return HttpResponse('service not found!')
+                return HttpResponse('access denied!')
         else:
-            return HttpResponse('access denied!')
+            return HttpResponse('table not found!')
 
 
 @csrf_exempt
@@ -92,6 +92,104 @@ def api_table_delete(request, table=None):
                 CrewEvent.addEvent(request, table.crew, u'Справочник ' + table.name + ' восстановлен')
             return HttpResponse('ok')
         else:
-            return HttpResponse('service not found!')
+            return HttpResponse('table not found!')
     else:
         return HttpResponse('access denied!')
+
+
+@csrf_exempt
+def api_field_list(request, table=None):
+    table = Table.objects.filter(id=table).first()
+    if table:
+        if not check_member(request.user, table.crew):
+            return JsonResponse({
+                'message': u'access denied!',
+                'data': ''
+            })
+        data = serializers.serialize('json', table.field_set.all().order_by('order'))
+        return JsonResponse({
+            'message': '',
+            'table': table.id,
+            'data': json.loads(data)
+        })
+    else:
+        return JsonResponse({
+            'message': u'Справочник не найден',
+            'model': ''
+        })
+
+
+@csrf_exempt
+def api_field_edit(request, field=None):
+    if request.method == 'GET':
+        field = Field.objects.filter(id=field).first()
+        if field:
+            if not check_member_admin(request.user, field.table.crew):
+                return JsonResponse({
+                    'message': u'access denied!',
+                    'data': ''
+                })
+            else:
+                data = serializers.serialize('json', [field,])
+                return JsonResponse({
+                    'message': '',
+                    'data': json.loads(data)
+                })
+        else:
+            return JsonResponse({
+                'message': u'Справочник не найден',
+                'data': ''
+            })
+    if request.method == 'POST':
+        if field == '0':
+            table = Table.objects.filter(id=request.POST.get('table', '0')).first()
+            if table:
+                if check_member_admin(request.user, table.crew):
+                    field = Field.objects.create(
+                        table=table,
+                        name=request.POST.get('name', '_'),
+                        type=request.POST.get('type', Field.TYPE_STRING),
+                        order=request.POST.get('order', 10),
+                    )
+                    field.save()
+                    CrewEvent.addEvent(request, table.crew, u'Добавлено новое поле ' + field.name +
+                                       ' в справочник ' + table.name)
+                    return HttpResponse('ok')
+                else:
+                    return HttpResponse('access denied!')
+            else:
+                return HttpResponse('table not found!')
+        field = Field.objects.filter(id=field).first()
+        if field:
+            if check_member_admin(request.user, field.table.crew):
+                field.name = request.POST.get('name', '_')
+                field.type = request.POST.get('type', Field.TYPE_STRING)
+                field.order = request.POST.get('order', 10)
+                field.save()
+                CrewEvent.addEvent(request, field.table.crew, u'Изменено ' + field.name +
+                                   ' в справочнике ' + field.table.name)
+                return HttpResponse('ok')
+            else:
+                return HttpResponse('access denied!')
+        else:
+            return HttpResponse('field not found!')
+
+
+@csrf_exempt
+def api_field_delete(request, field=None):
+    field = Field.objects.filter(id=field).first()
+    if field:
+        if check_member_admin(request.user, field.table.crew):
+            field.deleted = not field.deleted
+            field.save()
+            if field.deleted:
+                CrewEvent.addEvent(request, field.table.crew, u'Поле ' + field.name +
+                                   'в справочнике ' + field.table.name + ' помечено удаленным')
+            else:
+                CrewEvent.addEvent(request, field.table.crew, u'Поле ' + field.name +
+                                   'в справочнике ' + field.table.name + ' восстановлено')
+            return HttpResponse('ok')
+        else:
+            return HttpResponse('access denied!')
+    else:
+        return HttpResponse('field not found!')
