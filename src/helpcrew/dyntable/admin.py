28460a19fb4430952005fe4df15038c259b038d7
Sergey Lagovskiy <slagovskiy@gmail.com>
2017-09-17 22:13:31 +0700

    index and record admin

--- src/helpcrew/dyntable/admin.py
+++ src/helpcrew/dyntable/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Table, Field
+from .models import Table, Field, Index, Record
 
 
 class TableAdmin(admin.ModelAdmin):
@@ -16,5 +16,21 @@ class FieldAdmin(admin.ModelAdmin):
     readonly_fields = ()
 
 
+class IndexAdmin(admin.ModelAdmin):
+    list_display = ('num', 'table', 'deleted')
+    ordering = ('table','num')
+    exclude = ()
+    readonly_fields = ()
+
+
+class RecordAdmin(admin.ModelAdmin):
+    list_display = ('index', 'field', 'value')
+    ordering = ('index', 'field')
+    exclude = ()
+    readonly_fields = ()
+
+
 admin.site.register(Table, TableAdmin)
 admin.site.register(Field, FieldAdmin)
+admin.site.register(Index, IndexAdmin)
+admin.site.register(Record, RecordAdmin)
