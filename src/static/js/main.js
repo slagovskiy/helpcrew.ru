3d69dd52b9fed722762e02e7818700567bfc1190
Sergey Lagovskiy <slagovskiy@gmail.com>
2017-09-17 20:02:25 +0700

    password generator

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -5,5 +5,16 @@ function isValidEmailAddress(emailAddress) {
     return pattern.test(emailAddress);
 };
 
+
+function generatePassword(len) {
+    var length = len;
+    var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    var retVal = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+        retVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    return retVal;
+};
+
 $(document).ready(function() {
 });
