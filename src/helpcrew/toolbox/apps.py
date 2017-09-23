11bbf278d529d4deecfe2723fb13098d9d2b4f0f
Sergey Lagovskiy <slagovskiy@gmail.com>
2017-09-23 15:13:43 +0700

    global init

--- src/helpcrew/toolbox/apps.py
+++ src/helpcrew/toolbox/apps.py
@@ -6,10 +6,13 @@ class ToolboxConfig(AppConfig):
 
     def ready(self):
         from .models import Global
-        Global.add_not_exist('user_crew_limit', '3')    # ограничение на количество команд у пользователя
-        Global.add_not_exist('crew_user_limit', '10')   # ограничение на пользователей в команде
-        Global.add_not_exist('start_task', '15')        # начало работ по новой заявке
-        Global.add_not_exist('incident_time_one', '2')  # время на решение инцидента до оповещения диспетчера
-        Global.add_not_exist('incident_time_two', '6')  # время на решение инцидента до оповещения администратора
-        Global.add_not_exist('request_time_one', '24')  # время на решение заявки до оповещения диспетчера
-        Global.add_not_exist('request_time_two', '72')  # время на решение заявки до оповещения администратора
+        try:
+            Global.add_not_exist('user_crew_limit', '3')    # ограничение на количество команд у пользователя
+            Global.add_not_exist('crew_user_limit', '10')   # ограничение на пользователей в команде
+            Global.add_not_exist('start_task', '15')        # начало работ по новой заявке
+            Global.add_not_exist('incident_time_one', '2')  # время на решение инцидента до оповещения диспетчера
+            Global.add_not_exist('incident_time_two', '6')  # время на решение инцидента до оповещения администратора
+            Global.add_not_exist('request_time_one', '24')  # время на решение заявки до оповещения диспетчера
+            Global.add_not_exist('request_time_two', '72')  # время на решение заявки до оповещения администратора
+        except:
+            pass
