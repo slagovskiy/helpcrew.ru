393e2d8cd528f5d1955b2f80b73047b118edbcb7
Sergey Lagovskiy <slagovskiy@gmail.com>
2017-09-23 16:15:22 +0700

    getUserHostAddress

--- src/helpcrew/toolbox/utils.py
+++ src/helpcrew/toolbox/utils.py
@@ -1,11 +1,11 @@
 def getUserHostAddress(request):
     ip = ''
-    if request.META['HTTP_X_REAL_IP']:  # check ip from share internet
-        ip = request.META['HTTP_X_REAL_IP']
-    elif request.META['HTTP_CLIENT_IP']:    # check ip from share internet
-        ip = request.META['HTTP_CLIENT_IP']
-    elif request.META['HTTP_X_FORWARDED_FOR']:  # to check ip is pass from proxy
-        ip = request.META['HTTP_X_FORWARDED_FOR']
+    if request.META.get('HTTP_X_REAL_IP'):  # check ip from share internet
+        ip = request.META.get('HTTP_X_REAL_IP')
+    elif request.META.get('HTTP_CLIENT_IP'):    # check ip from share internet
+        ip = request.META.get('HTTP_CLIENT_IP')
+    elif request.META.get('HTTP_X_FORWARDED_FOR'):  # to check ip is pass from proxy
+        ip = request.META.get('HTTP_X_FORWARDED_FOR')
     else:
-        ip=request.META['REMOTE_ADDR'];
+        ip = request.META.get('REMOTE_ADDR')
     return ip
